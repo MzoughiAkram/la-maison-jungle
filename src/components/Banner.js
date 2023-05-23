e1d22c8332554a1e522a65c05be8da5061a59f99
MzoughiAkram <donman81@hotmail.fr>
2023-05-23 20:12:25 +0100

    Ajout du styles&assets à l'application

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+function Banner() {
+    return <div className = "lmj-banner">
+    <h1><img src={logo} alt='La maison jungle' className='lmj-logo' />
+                La maison jungle</h1>
+            </div>
+}
+
+export default Banner
