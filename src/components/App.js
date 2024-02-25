0cbbde6ee39dfcede72048ddc7216569655fa935
MzoughiAkram <donman81@hotmail.fr>
2024-02-25 20:07:44 +0100

    utilistation de useState et useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
 import logo from '../assets/logo.png'
 import '../styles/App.css'
+import '../styles/Layout.css'
 import Banner from './Banner'
-//import Cart from './Cart'
+import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import QuestionForm from './QuestionForm'
 import Footer from './Footer'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const savedCart = localStorage.getItem('cart')
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+  useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+  
+
   return  (<div>
               <Banner>
 				        <img src={logo} alt='La maison jungle' className='lmj-logo' />
 				        <h1 className='lmj-title'>La maison jungle</h1>
 			        </Banner>
-			        {/* <Cart /> */}
-			        <ShoppingList />
+             
+			        <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+              </div>
+			        
               <QuestionForm />
               <Footer />
 
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,83 @@
+import { useState, useEffect } from 'react';
 import '../styles/Banner.css'
-function Cart() {
+import '../styles/Cart.css'
+/*function Cart() {
     const monsteraPrice = 8
     const ivyPrice = 10
-    const flowerPrice = 15
-    var Total = monsteraPrice + ivyPrice + flowerPrice
-    return (<div className="lmj-cart">
+    const flowerPrice = 15*/
+    /*var Total = monsteraPrice + ivyPrice + flowerPrice*/
+   /* const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
+    <div className="lmj-cart">
     <h2>Panier</h2>
     <ul>
      <li>Monstera : {monsteraPrice}dt</li>
+      <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+      </button>
      <li>Lierre : {ivyPrice}dt</li>
      <li>Fleurs : {flowerPrice}dt</li>
     </ul>
-      Total : { Total }dt
-      </div>)
+    <h3>Total : {monsteraPrice * cart}€</h3>
+    <div><button onClick={() => updateCart(0)}>
+                    vider le panier
+      </button></div>
+      </div>) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+    )
 }
+export default Cart*/
+
+function Cart({ cart, updateCart }) {
+	const [isOpen, setIsOpen] = useState(true)
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
+	)
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	},[total])	
+	return isOpen ? (
+		<div className='lmj-cart'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+								
+								<button onClick={() => updateCart([])}>Supprimer</button>
+								
+							</div>
+						))}
+						
+					</ul>
+					<h3>Total : {total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
+		</div>
+	) : (
+		<div className='lmj-cart-closed'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(true)}
+			>
+				Ouvrir le Panier
+			</button>
+		</div>
+	)
+}
+
 export default Cart
